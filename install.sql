8bc2bdadcf3a37890832755799213733c6d9e13c
Spiderlogical <27599581+xSpiderMC@users.noreply.github.com>
2025-02-28 20:56:59 -0800

    tweak(sql): modify information saved

--- install.sql
+++ install.sql
@@ -1,33 +1,30 @@
 CREATE TABLE IF NOT EXISTS `splg_hunting_users` (
-    `identifier` VARCHAR(100) NOT NULL,
-    `tasks_completed` INT(11) NOT NULL DEFAULT 0,
+    `user_id` VARCHAR(100) NOT NULL,
+    `tasks_completed` INT(11) UNSIGNED NOT NULL DEFAULT 0,
     `level` INT(11) NOT NULL DEFAULT 1,
     `xp` INT(11) NOT NULL DEFAULT 0,
-    PRIMARY KEY (`identifier`)
+    PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `splg_hunting_tasks` (
-    `id` INT(11) NOT NULL,
-    `identifier` VARCHAR(100) NOT NULL PRIMARY KEY,
+    `id` INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `user_id` VARCHAR(100) NOT NULL,
     `name` VARCHAR(100) NOT NULL,
-    `description` TEXT NOT NULL,
     `xp_reward` INT(11) UNSIGNED NOT NULL DEFAULT 0,
     `money_reward` INT(11) UNSIGNED NOT NULL DEFAULT 0,
     `requirements` LONGTEXT NOT NULL COLLATE utf8mb4_bin DEFAULT JSON_OBJECT() CHECK (JSON_VALID(`requirements`)),
-    `completed` TINYINT(1) NOT NULL DEFAULT 0
+    `progress` LONGTEXT NOT NULL COLLATE utf8mb4_bin DEFAULT JSON_OBJECT() CHECK (JSON_VALID(`progress`)),
+    `completed` TINYINT(1) NOT NULL DEFAULT 0,
+    CONSTRAINT `user_id_fk`  FOREIGN KEY (`user_id`) REFERENCES `splg_hunting_users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DELIMITER //
-
-CREATE TRIGGER after_task_completed
+CREATE TRIGGER IF NOT EXISTS after_task_completed
 AFTER UPDATE ON `splg_hunting_tasks`
 FOR EACH ROW
 BEGIN
     IF NEW.completed = 1 AND OLD.completed = 0 THEN
         UPDATE `splg_hunting_users`
         SET `tasks_completed` = `tasks_completed` + 1
-        WHERE `identifier` = NEW.identifier;
+        WHERE `user_id` = NEW.user_id;
     END IF;
-END//
-
-DELIMITER ;
+END;
