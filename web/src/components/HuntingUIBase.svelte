38b4fc614263448588cde603a640c75c1d0568fb
Spiderlogical <27599581+xSpiderMC@users.noreply.github.com>
2025-03-17 01:36:15 -0700

    chore(web): prettier config, working on scaling

--- web/src/components/HuntingUIBase.svelte
+++ web/src/components/HuntingUIBase.svelte
@@ -1,33 +1,61 @@
 <script lang="ts">
-    import { AppBar } from '@skeletonlabs/skeleton'; 
-    import HuntingIcon from '@components/HuntingIcon.svelte';
-    import { LOCALE } from '@stores/ui';
-    import IcBaselineHome from '~icons/ic/baseline-home';
-    import IcBaselinePerson from '~icons/ic/baseline-person'; 
-    import { path } from 'elegua';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import HuntingIcon from '@components/HuntingIcon.svelte';
+	import { LOCALE } from '@stores/ui';
+	import IcBaselineHome from '~icons/ic/baseline-home';
+	import IcBaselinePerson from '~icons/ic/baseline-person';
+	import { path } from 'elegua';
 
-    $: classesActive = (href: string) => {
-        return $path === href ? 'variant-soft-primary' : '' };
+	$: classesActive = (href: string) => {
+		return $path === href ? 'variant-soft-primary' : '';
+	};
 </script>
 
-<div class="flex flex-auto justify-center items-center h-screen">
-    <div class="flex flex-col w-2/3 h-3/4 bg-surface-50-900-token rounded-container-token">
-        <AppBar class="rounded-tl-container-token rounded-tr-container-token p-2" gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-self-end self-center">
-            <svelte:fragment slot="lead">
-                <!-- <img src={huntingIcon} alt="Hunting Icon" class="w-14 h-14"/> -->
-                <HuntingIcon class="w-14 h-14 fill-token"/>   
-            </svelte:fragment>
-            
-            <h1 class="h1">{$LOCALE.HUNTING_LABEL}</h1>
+<div class="flex h-screen flex-auto items-center justify-center">
+	<div
+		class="bg-surface-50-900-token rounded-container-token mx-auto flex h-[80vh] w-full max-w-[90vw] flex-col md:max-w-[85vw] lg:max-w-[80vw] xl:max-w-[75vw] 2xl:max-w-[70vw]"
+	>
+		<AppBar
+			class="rounded-tl-container-token rounded-tr-container-token p-2 md:p-3 xl:p-4"
+			gridColumns="grid-cols-3"
+			slotDefault="place-self-center"
+			slotTrail="place-self-end self-center"
+		>
+			<svelte:fragment slot="lead">
+				<HuntingIcon
+					class="fill-token h-[3.5rem] w-[3.5rem] xl:h-[4rem] xl:w-[4rem] 2xl:h-[4.5rem] 2xl:w-[4.5rem]"
+				/>
+			</svelte:fragment>
 
-            <svelte:fragment slot="trail">
-                <div class="inline-flex gap-4">
-                    <a href="/" class="flex flex-row gap-1 items-center rounded-container-token p-2 {classesActive('/')}"> <IcBaselineHome/> <span>{$LOCALE.HOME_LABEL}</span></a>
-                    <a href="/profile" class="flex flex-row gap-1 items-center rounded-container-token p-2 {classesActive('/profile')}"> <IcBaselinePerson/> <span>{$LOCALE.PROFILE_LABEL}</span></a>
-                </div>
-            </svelte:fragment>
-        </AppBar>
-        
-        <slot />
-    </div>
+			<h1 class="h1 text-[clamp(1.5rem,2vw,3rem)]">
+				{$LOCALE.HUNTING_LABEL}
+			</h1>
+
+			<svelte:fragment slot="trail">
+				<div class="inline-flex gap-4">
+					<a
+						href="/"
+						class="rounded-container-token flex flex-row items-center gap-1 p-2 {classesActive(
+							'/',
+						)}"
+					>
+						<IcBaselineHome class="h-[1em] w-[1em]" />
+						<span>{$LOCALE.HOME_LABEL}</span>
+					</a>
+					<a
+						href="/profile"
+						class="rounded-container-token flex flex-row items-center gap-1 p-2 {classesActive(
+							'/profile',
+						)}"
+					>
+						<IcBaselinePerson class="h-[1em] w-[1em]" />
+						<span>{$LOCALE.PROFILE_LABEL}</span>
+					</a>
+				</div>
+			</svelte:fragment>
+		</AppBar>
+		<div class="flex-1 overflow-auto p-4">
+			<slot />
+		</div>
+	</div>
 </div>
--- web/src/components/TaskCard.svelte
+++ web/src/components/TaskCard.svelte
@@ -1,45 +1,51 @@
 <script lang="ts">
-    import { Send } from "@enums/events";
-    import { TASK_BUTTONS_VISIBLE } from "@stores/ui"
-    import { SendEvent } from "@utils/eventsHandlers";
-    import { LOCALE } from "@stores/ui";
-    export let taskId: number;
-    export let title: string;
-    export let cashReward: number;
-    export let xpReward: number;
-    export let canClaim: boolean;
-    export let requirements: string[] = [];
+	import { ProgressBar } from '@skeletonlabs/skeleton';
+	import { Send } from '@enums/events';
+	import { TASK_BUTTONS_VISIBLE } from '@stores/ui';
+	import { SendEvent } from '@utils/eventsHandlers';
+	import { LOCALE } from '@stores/ui';
+	export let taskId: number;
+	export let title: string;
+	export let cashReward: number;
+	export let xpReward: number;
+	export let canClaim: boolean;
+	export let requirements: string[] = [];
 
-    function handleTaskButtonClick() {
-        SendEvent(Send.claimTask, taskId);
-    }
-
+	function handleTaskButtonClick() {
+		SendEvent(Send.claimTask, taskId);
+	}
 </script>
 
 <div class="flex flex-col card variant-soft-surface w-1/4 xl:w-2/5">
-    <header class="card-header flex flex-row place-content-center">
-        <h2 class="h2">{title}</h2>
-        <span class="ml-auto">
-            <h3 class="h3 text-success-600">${cashReward}</h3>
-            <h4 class="h4 text-success-600">{xpReward}XP</h4>
-        </span>
-    </header>
-    <section class="py-4 pl-4 place-self-start">
-        {#if requirements.length > 0}
-        <h3 class="h3"> {$LOCALE.REQUIREMENTS_LABEL} </h3>
-        <ul class="list">
-            {#each requirements as requirement}
-                <li>
-                    <span>&#x2022</span>
-                    <span class="flex-auto">{requirement}</span>
-                </li>
-            {/each}
-        </ul>
-        {/if}
-    </section>
-    {#if $TASK_BUTTONS_VISIBLE}
-    <footer class="card-footer ml-auto">
-        <button class="btn btn-lg variant-soft-primary" disabled={!canClaim} on:click={handleTaskButtonClick}>{$LOCALE.BUTTON_COMPLETE_LABEL}</button>
-    </footer>
-    {/if}
+	<header class="card-header flex flex-row place-content-center">
+		<h2 class="h2">{title}</h2>
+		<span class="ml-auto">
+			<h3 class="h3 text-success-600">${cashReward}</h3>
+			<h4 class="h4 text-success-600">{xpReward}XP</h4>
+		</span>
+	</header>
+	<section class="py-4 pl-4 place-self-start">
+		{#if requirements.length > 0}
+			<h3 class="h3">{$LOCALE.REQUIREMENTS_LABEL}</h3>
+			<ul class="list">
+				{#each requirements as requirement}
+					<li>
+						<span>&#x2022</span>
+						<span class="flex-auto">{requirement}</span>
+					</li>
+				{/each}
+			</ul>
+		{/if}
+	</section>
+	{#if $TASK_BUTTONS_VISIBLE}
+		<footer class="card-footer flex items-center gap-4">
+			<ProgressBar value={0} />
+			<button
+				class="btn btn-lg variant-soft-primary ml-auto"
+				disabled={!canClaim}
+				on:click={handleTaskButtonClick}
+				>{$LOCALE.BUTTON_COMPLETE_LABEL}</button
+			>
+		</footer>
+	{/if}
 </div>
